798473b76188ee058d684a0c7c61102d6049a441
KevinYakha <kevin@yakha.eu>
2024-04-16 14:01:53 +0200

    Projektdateien hinzufügen.

---
+++ SimpleLinkedList.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class SimpleLinkedList<T>
+{
+    public int Count => throw new NotImplementedException("You need to implement this function.");
+    
+    public void Push(T value)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public T Pop()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
